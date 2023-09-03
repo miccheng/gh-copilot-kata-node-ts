22cbb451beb14840133df5526f7508306f3c14b3
Michael Cheng <mcheng.work@gmail.com>
2023-09-03 21:54:06 +0800

    First implementation of the solution.

---
+++ FizzBuzz.ts
@@ -0,0 +1,15 @@
+export default function FizzBuzz(n: number): Array<number | string> {
+  const result: Array<number | string> = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
